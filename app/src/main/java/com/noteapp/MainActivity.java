f9534ab59ff213bdd457eccef209f944c18073de
kalin <kalindusupun@gmail.com>
2021-08-10 13:26:47 +0530

    Note App upload

---
+++ app/src/main/java/com/noteapp/MainActivity.java
@@ -0,0 +1,16 @@
+ package com.noteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+ public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      //  getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
