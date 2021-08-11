2e754b05a41a25aacb891cd351db64c5bb8182ac
kalin <kalindusupun@gmail.com>
2021-08-11 18:01:48 +0530

    Note App upload

--- app/src/main/java/com/noteapp/FirebaseNoteAdapter.java
+++ app/src/main/java/com/noteapp/FirebaseNoteAdapter.java
@@ -1,16 +1,21 @@
 package com.noteapp;
 
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+import java.util.Random;
 
 public class FirebaseNoteAdapter extends FirestoreRecyclerAdapter<FirebaseModel,FirebaseNoteAdapter.NoteViewHolder> {
     /**
@@ -25,8 +30,13 @@ public class FirebaseNoteAdapter extends FirestoreRecyclerAdapter<FirebaseModel,
 
     @Override
     protected void onBindViewHolder(@NonNull NoteViewHolder holder, int position, @NonNull FirebaseModel model) {
+
         holder.mTitle.setText(model.getTitle());
         holder.noteContent.setText(model.getContent());
+
+        Random rnd = new Random();
+        int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+        holder.noteCard.setBackgroundColor(color);
     }
 
     @NonNull
@@ -39,14 +49,14 @@ public class FirebaseNoteAdapter extends FirestoreRecyclerAdapter<FirebaseModel,
 
     public class NoteViewHolder extends RecyclerView.ViewHolder {
        private TextView mTitle,noteContent;
-        LinearLayout mNotes;
+        CardView noteCard;
 
         public NoteViewHolder(@NonNull View itemView) {
             super(itemView);
 
             mTitle =itemView.findViewById(R.id.noteTitle);
             noteContent = itemView.findViewById(R.id.noteContent);
-            mNotes = itemView.findViewById(R.id.note);
+            noteCard = itemView.findViewById(R.id.noteCard);
         }
     }
 }
